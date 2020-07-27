8c9c33848999b86bfe8136d4bfb113e208abf7f8
Damodar86 <tdamodar88@gmail.com>
2020-07-27 10:21:10 +0530

    modified Slf4j added

--- src/main/java/com/kurnool/librarysystem/service/LoginService.java
+++ src/main/java/com/kurnool/librarysystem/service/LoginService.java
@@ -1,13 +1,15 @@
 package com.kurnool.librarysystem.service;
 
 import com.kurnool.librarysystem.model.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class LoginService {
 
     public String login(LoginRequest loginRequest) {
-        System.out.println(loginRequest.getUsername());
-        return "Success";
+        log.info("successfuly login");
+        return "Successfully logedin";
     }
 }
